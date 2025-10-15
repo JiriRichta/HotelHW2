63d860e5271ba41ea94ab0d14852796e2416b279
richta <jir.rich@seznam.cz>
2025-10-15 17:03:57 +0200

    dodělávám výstup v mainu .. chybí seznamy a vyřešit vytváře ní guest listu

--- src/cz/hotel/hotelhw2/Booking.java
+++ src/cz/hotel/hotelhw2/Booking.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import cz.hotel.hotelhw2.Guest;
-import cz.hotel.hotelhw2.Room       ;
+import cz.hotel.hotelhw2.Room;
 
 public class Booking {
     private Guest guest;
     private Room room;
-    private List otherGuests= new ArrayList<>();
+    private List <Guest>otherGuests = new ArrayList<>();
     private LocalDate arrival;
     private LocalDate departure;
     private TypeOfVacation typeOfVacation;
@@ -18,26 +18,71 @@ public class Booking {
 
     //region constructors
     //variata pro jednoho hosta
-    public Booking(Guest guest, Room room, LocalDate arrival, LocalDate departure, TypeOfVacation typeOfVacation){
+    public Booking(Guest guest, Room room, LocalDate arrival, LocalDate departure, TypeOfVacation typeOfVacation) {
         this.guest = guest;
-        this.room= room;
+        this.room = room;
         this.arrival = arrival;
-        this.departure= departure;
+        this.departure = departure;
         this.typeOfVacation = typeOfVacation;
     }
-
     //end region constructors
 
     //region gettery a settery
 
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public List<Guest> getOtherGuests() {
+        return otherGuests;
+    }
+
+    public void addOtherGuests(List<Guest> otherGuests) {
+        this.otherGuests = otherGuests;
+    }
+
+    public LocalDate getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(LocalDate arrival) {
+        this.arrival = arrival;
+    }
+
+    public LocalDate getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(LocalDate departure) {
+        this.departure = departure;
+    }
+
+    public TypeOfVacation getTypeOfVacation() {
+        return typeOfVacation;
+    }
+
+    public void setTypeOfVacation(TypeOfVacation typeOfVacation) {
+        this.typeOfVacation = typeOfVacation;
+    }
 
 
     //end region gettery a settery
 
     //regin enum
-    public enum TypeOfVacation{
+    public enum TypeOfVacation {
         WORK, HOLIDAY
     }
     //end region enum
-
 }
--- src/cz/hotel/hotelhw2/Main.java
+++ src/cz/hotel/hotelhw2/Main.java
@@ -1,13 +1,24 @@
 package cz.hotel.hotelhw2;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Guest guest1 = new Guest("Franta","vocas", LocalDate.of(1988,5,6));
+    public static void main (String[] args) {
+        Guest guest1 = new Guest("Adéla","Malíková", LocalDate.of(1993,3,13));
+        Guest guest2 = new Guest("Jan", "Dvořáček",LocalDate.of(1995,5,5));
 
-        System.out.println(guest1.getName()+" "+guest1.getSurname());
+        guest2.setBirthDate(LocalDate.of(1955,4,5));
+
+        System.out.println(guest2.getName()+" "+guest2.getSurname()+" ("+ guest2.getBirthDate()+")");
+
+        Room room1 = new Room(1,1,true,true,new BigDecimal("1000"));
+        Room room2 = new Room(2,1,true,true,new BigDecimal("1000"));
+        Room room3 = new Room(3,3,false,true,new BigDecimal("2400"));
+
+        Booking booking1 = new Booking(guest1,room1,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26), Booking.TypeOfVacation.WORK);
+        Booking booking2 = new Booking(guest1,room3,LocalDate.of(2021,9,1),LocalDate.of(2021,9,14), Booking.TypeOfVacation.HOLIDAY);
+        Booking booking3 = new Booking(guest2,room3,LocalDate.of(2021,9,1),LocalDate.of(2021,9,14), Booking.TypeOfVacation.HOLIDAY);
+
     }
 }
